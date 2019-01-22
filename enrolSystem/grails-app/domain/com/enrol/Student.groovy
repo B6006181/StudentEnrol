d538a6c84024d5ed8b354ef49ec4827468853f8b
Bally <bally@B6006181.localdomain>
2019-01-22 17:41:44 +0000

    Added student

---
+++ enrolSystem/grails-app/domain/com/enrol/Student.groovy
@@ -0,0 +1,22 @@
+package com.enrol
+
+class Student {
+String studentname
+String studentID
+Date dob
+Boolean isFundingAvailable
+String studentEmail
+String studentUsername
+String studentPassword
+String course
+    static constraints = {
+studentname blank:false, nullable:false
+studentID blank:false, nullable:false
+dob blank:false, nullable:false
+isFundingAvailable blank:false, nullable:false
+studentEmail blank:false, nullable:false, email:true
+studentUsername blank:false, nullable:false
+studentPassword blank:false, nullable:false
+course blank:false, nullable:false
+    }
+}
