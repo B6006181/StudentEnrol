819510473fd9ffb4d59b51657b8db486ea3679cb
Bally <bally@B6006181.localdomain>
2019-01-22 16:56:10 +0000

    grails

---
+++ enrolSystem/grails-app/domain/com/enrol/Course.groovy
@@ -0,0 +1,26 @@
+package com.enrol
+
+class Course {
+String department;
+String courseTitle
+String courseLeader
+String courseCode
+Date startDate
+Date endDate
+String description
+int numberOfStudents
+double tuitionFees
+String studyMode
+    static constraints = {
+courseTitle blank:false, nullable:false
+department blank:false, nullable:false
+courseLeader blank:false, nullable:false
+courseCode blank:false, nullable:false
+numberOfStudents blank:false, nullable:false, minsize:20, maxsize:60
+startDate blank:false, nullable:false
+endDate blank:false, nullable:false
+studyMode nullable:false, minsize:20;
+description nullable:false, maxsize:5000, widget:'textarea'
+tuitionFees nullable:false, blank:false, scale:2
+    }
+}
